47e83da9e8b99798247bcd8605b99e9312f6bb5e
banaszkiewicz-piotr <banaszkiewicz.piotr@zst.radom.pl>
2025-10-17 10:23:05 +0200

    Quiz class

---
+++ MyApplication/app/src/main/java/classes/Quiz.java
@@ -0,0 +1,5 @@
+package classes;
+
+public class Quiz {
+    public String[][] questions = {{"Jakie miasto jest stolicą Hiszpanii?", "Madryt"}, {"Ile nóg ma pająk?", "8"}, {"Ile jest języków urzędowych w Szwajcarii?", "4"}, {"Czym Helge Meyer dostarczał pomoc humanitarną do obleganych miast podczas wojny w Jugosławii?", "Chevroletem Camaro"}, {"Co jaki okres czasu mamy rok przestępny?", "4 lata"}, {"Ile lat ma Maryla Rodowicz?", "Wszystkie"}};
+}
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.myapplication;
 
+import static android.view.View.INVISIBLE;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +17,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        super.onCreate(savedInstanceState);
+        LinearLayout start = findViewById(R.id.start);
+        Button btnStart = findViewById(R.id.button);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start.setVisibility(INVISIBLE);
+            }
+        });
     }
 }
