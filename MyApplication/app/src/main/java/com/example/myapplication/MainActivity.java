d74eba101811cbeb474bfe09506a3c865703d776
banaszkiewicz-piotr <banaszkiewicz.piotr@zst.radom.pl>
2025-11-07 10:22:12 +0100

    Quiz

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.myapplication;
 
 import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
 
+import android.opengl.Visibility;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,18 +16,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import classes.Quiz;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        LinearLayout start = findViewById(R.id.start);
         Button btnStart = findViewById(R.id.button);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        TextView pytanie = findViewById(R.id.pytanie);
+        Quiz quiz = new Quiz();
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                start.setVisibility(INVISIBLE);
+                btnStart.setVisibility(INVISIBLE);
+                button1.setVisibility(VISIBLE);
+                button2.setVisibility(VISIBLE);
+                button3.setVisibility(VISIBLE);
+                pytanie.setVisibility(VISIBLE);
+                quiz.Losuj();
             }
         });
+
+
     }
 }
